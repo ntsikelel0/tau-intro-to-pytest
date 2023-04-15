455dbabf4663ad3f99561d1b81f7eb8b8bd2277e
Ntsikelelo Sonjica <n.sonjica@gmail.com>
2023-04-15 18:36:39 +0200

    revisit pytest

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,8 +1,14 @@
+# A test that verifies whether 1 + 1 is 2
 def test_one_plus_one():
         assert 1 + 1 == 2
 
+# A test that verifies whether a sum of 2 integer variables is equal to that integer value assigned to another variable 
 def test_one_plus_two():
         a = 1
         b = 2
         c = 3
         assert a + b == c
+    
+#A test that verifies an exception
+def test_devide_by_zero():
+        num = 1 / 0
